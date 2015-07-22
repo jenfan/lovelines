0b20f8b5f1c41de722594e2da5d91d91ab29d0d9
Gregor <am@iworry.ru>
2015-07-22 20:41:22 +0300

    fucken shit

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ ssh_options[:forward_agent] = true
 
 # Имя вашего проекта в панели управления.
 # Не меняйте это значение без необходимости, оно используется дальше.
-set :application,     "lovelines"
+set :application,     "loveliness"
 
 # Сервер размещения проекта.
 set :deploy_server,   "calcium.locum.ru"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
-lovelines.application.configure do
-# Rails.application.configure do
+# lovelines.application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
