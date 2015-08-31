dbdf79098c2e34d59db0947b4224734bab2c0f24
Евгений <am@iworry.ru>
2015-08-31 13:25:12 +0300

    video fix

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,6 @@ class Video < ActiveRecord::Base
 	before_update :all_index_to_nil
 
 	def all_index_to_nil
-		Video.update_all(to_index:false)
+		Video.update_all(to_index:false) if to_index
 	end
 end
